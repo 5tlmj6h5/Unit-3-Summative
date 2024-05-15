499558f3baf81fb3b0a303b84f31f8ec49b1e466
张伟 <xhexhur@gmail.com>
2024-05-15 19:30:16 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+/**
+ * ICS3UC - Mr. Brash 🐿️
+ *  
+ * Unit 3 (Loops) Summative Task
+ * 
+ * Read the README.md file (right-click it and select Open Preview)
+ * Collaboration is encouraged in terms of helping each other LEARN.
+ * Cheating (copying each other) is not.
+ * 
+ * Author: 
+ */
+
+'use strict'
+
+// To get a number as money, use: dollar.format(value)
+// For example:  dollar.format(3.4) will create the string $3.40
+// This is specifically for the "receipt" function
+let dollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+
+function bracelets(sales) {
+
+}
+
+function tickets(night) {
+
+}
+
+function bouncer(guests, night) {
+
+}
+
+function receipt(items, prices) {
+
+}
+
+function letter_count(str) {
+
+}
